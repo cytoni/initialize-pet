030590d0a587c881c02e3cfc29b32df78718a6fa
cytoni <113114304+cytoni@users.noreply.github.com>
2022-09-27 11:01:34 +0200

    Update

---
+++ src/Hello.jsx
@@ -0,0 +1,10 @@
+//Komponente in React IMMER mit Grossbuchstaben beginnen
+const Hello = () => {
+    //Gibt ein H1 element zurück
+    return <div>
+        <h1>Hallo Welt</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero velit sed molestiae saepe explicabo amet earum iusto dicta culpa, eum magni, fugiat dolorum aut repellendus. Iste tenetur nobis placeat excepturi.</p>
+        </div>;
+}
+//Exportiert die Komponente
+export default Hello;
---
+++ src/ageConverter.js
@@ -0,0 +1,4 @@
+const ageConverter = (birthday) => {
+  const age = Math.floor((Date.now() - new Date(birthday)) / 1000 / (60 * 60 * 24 * 365));
+  return age <= 1 ? '1 Jahr' : `${age} Jahre`;
+};
